66b135c94de202ae3eca81b3b309bf44a67b0839
ssam <ssam8610@naver.com>
2019-09-26 12:55:38 +0900

    echo 완성

---
+++ src/main/java/dbot/MainApp.java
@@ -0,0 +1,21 @@
+package dbot;
+
+import listener.LunchListener;
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class MainApp {
+	public static void main(String[] args) {
+		JDABuilder builder = new JDABuilder(AccountType.BOT);
+		String token = "NjI2NjA5NzE0MjQzMDQzMzQy.XYwnxA.5q3ASgtl2eBxl3jLf0xmLRjznFE";
+		builder.setToken(token);
+		
+		try {
+			builder.addEventListeners(new LunchListener());
+			builder.build();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
